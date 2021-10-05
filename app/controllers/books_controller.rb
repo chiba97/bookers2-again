291e4aaf57bfc93bcc3da06ab416f97e8ffeb6b1
chiba97 <ktfkyrya@gmail.com>
2021-10-05 13:30:54 +0000

    add エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,53 @@
 class BooksController < ApplicationController
-  
+
   def index
     @user = current_user
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to books_path
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
-  
+
   def show
     @new_book = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
   end
-  
-  
-  private 
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
+  private
   def book_params
     params.require(:book).permit(:title,:body)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,18 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user)
+    end
   end
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
   
   private
