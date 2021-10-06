62182052e6ac521220e1d08a9f9d32601f84411a
chiba97 <ktfkyrya@gmail.com>
2021-10-06 02:32:30 +0000

    create finish  bookers2-again

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
       @user = current_user
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-       redirect_to book_path(@book)
+       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render :edit
     end
